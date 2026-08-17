2910e9b7815d74c8f07df346a06597f7b851799c
lazy <lazywc@gmail.com>
2026-08-17 13:07:02 -0700

    T24: use exact durian title in cluster test

--- tests/cluster.test.js
+++ tests/cluster.test.js
@@ -139,7 +139,7 @@ test("six 36kr half-year earnings stay six cards", () => {
 });
 
 test("durian same title weibo+baidu stay one card", () => {
-  const title = "\u69df\u83b2\u4ef7\u683c\u5f7b\u5e95\u5d29\u4e86";
+  const title = "槔莲价格彻底崩了";
   const cards = cluster([
     { id: "weibo:d", title, source: "weibo" },
     { id: "baidu:d", title, source: "baidu" },

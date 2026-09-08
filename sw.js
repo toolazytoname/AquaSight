8ebcdd3eba19fd38cb692f068352f5a7f292f131
toolazytoname <toolazytoname@users.noreply.github.com>
2026-09-08 07:45:39 +0000

    deploy: 04d09db576e4d60908aba670c900f748fc76c330

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL = "aquasight-shell-v5";
-const PRIVATE = "aquasight-private-v5";
+const SHELL = "aquasight-shell-v6";
+const PRIVATE = "aquasight-private-v6";
 const SHELL_URLS = [
   "./",
   "./index.html",
